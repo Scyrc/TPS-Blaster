9d17eb47f6521ce16127841ce21ef15e8c8e3729
yrc <419107280@qq.com>
2023-03-22 18:17:30 +0800

    select match type&end

--- Source/Blaster/GameMode/LobbyGameMode.cpp
+++ Source/Blaster/GameMode/LobbyGameMode.cpp
@@ -2,20 +2,41 @@
 
 
 #include "LobbyGameMode.h"
+
+#include "MultiplayerSessionsSubsystem.h"
 #include "GameFramework/GameStateBase.h"
 
 void ALobbyGameMode::PostLogin(APlayerController* NewPlayer)
 {
 	Super::PostLogin(NewPlayer); 
 	int32 NumberOfPlayers = GameState.Get()->PlayerArray.Num();
-
-	if (NumberOfPlayers == PlayersNum)
+	UGameInstance* GameInstance = GetGameInstance();
+	if(GameInstance)
 	{
-		UWorld* World = GetWorld();
-		if (World)
+		UMultiplayerSessionsSubsystem* Subsystem = GameInstance->GetSubsystem<UMultiplayerSessionsSubsystem>();
+		check(Subsystem);
+
+		if (NumberOfPlayers == Subsystem->DesiredNumPublicConnections)
 		{
-			bUseSeamlessTravel = true;
-			World->ServerTravel(FString("/Game/maps/TestLevel?listen"));
+			UWorld* World = GetWorld();
+			if (World)
+			{
+				bUseSeamlessTravel = true;
+				FString MatchType = Subsystem->DesiredMatchType;
+				if(MatchType == "FreeForAll")
+				{
+					World->ServerTravel(FString("/Game/maps/BlasterMap?listen"));
+				}
+				else if(MatchType == "Teams")
+				{
+					World->ServerTravel(FString("/Game/maps/Teams?listen"));
+				}
+				else if(MatchType == "CaptureTheFlag")
+				{
+					World->ServerTravel(FString("/Game/maps/CaptureTheFlag?listen"));
+				}
+			}
 		}
 	}
+	
 }
